988a6b1f95092580d7fa5d4a50d41fdfbacbb837
15038791997 <15038791997@163.com>
2025-09-05 21:50:33 +0800

    feat(log):日志模块-format解析半部分

--- sylar/log.cc
+++ sylar/log.cc
@@ -7,6 +7,24 @@ Logger::Logger(const std::string& name = "root"){
 
 }
 
+std::string LogLevel::ToString(LogLevel::Level level){
+    switch (level){
+#define XX(name) \
+        case LogLevel::name: \
+            return #name; \
+            break;
+
+    XX(DEBUG);
+    XX(INFO);
+    XX(WARN);
+    XX(ERROR);
+    XX(FATAL);
+#undef XX
+        default:
+            return "UNKNOWN";
+    }
+}
+
 
 void Logger::addAppender(LogAppender::ptr appender){
     m_appenders.push_back(appender);
@@ -58,7 +76,99 @@ void StdoutLogAppender::log(LogLevel::Level level, LogEvent::ptr event){
     } 
 }
 
+LogFormatter::LogFormatter(const std::string& pattern)
+    : m_pattern(pattern){
+}
 
+std::string LogFormatter::format(LogLevel::ptr level, LogEvent::prt event){
+    std::stringstream ss;
+    for(auto& i : m_items){
+        i->format(ss, level,event);
+    }
+    return ss.str();
+}
+//日志格式解析函数
+void LogFormatter::init(){
+    std::vector<std::tuple<std::string, std::string, int>> vec;     //连续字符串种类，格式记录
+    std::string nstr;                                               //存储连续字符串
+    int fmt_status = 0;         //字符状态
+    for(size_t i = 0; i < m_pattern.size(); i++){
+        if(m_pattern[i] == '%' && !nstr.empty()){
+            //添加上个vec记录
+            if(!nstr.empty()){
+                vec.push_back(std::make_tuple(nstr, std::string(), fmt_status));
+            }
+            //记录当前格式状态
+            nstr = "";
+            fmt_status = 1;
+            //添加当前格式记录
+            if ((++i) >= m_pattern.size()) {break;}
+            //特殊格式
+            if(m_pattern[i] == 'd'){
+                //循环后续格式
+                bool is_in = false;
+                std::string time_str = "";
+                while (++i && i < m_pattern.size()){
+                    if(m_pattern[i] == '{'){
+                        is_in = true;
+                    }
+                    time_str.append(m_pattern[i]);
+                    if(is_in && m_pattern[i] == '}'){
+                        //遇见}循环跳出
+                        break;
+                    }
+                }
+                if(i < m_pattern.size()){
+                    vec.push_back(std::make_tuple(std::string("d"), time_str, fmt_status));
+                }
+            }
+            else if(m_pattern[i] == '%'){
+                vec.push_back(std::make_tuple(m_pattern[i], std::string("%"), fmt_status));
+            }
+            else if(!isalpha(m_pattern[i])){
+                //其他格式
+                vec.push_back(std::make_tuple(m_pattern[i], std::string(), fmt_status));
+            }
+            else{
+                //未定义格式
+                vec.push_back(std::make_tuple(m_pattern[i], std::string(), 0));
+            }
+            //状态恢复
+            nstr = "";
+            fmt_status = 0;
+        }
+        else{
+            nstr.append(m_pattern[i]);      //加到临时字符
+        }
+    }
+    if(!nstr.empty()){
+        vec.push_back(std::make_tuple(nstr, std::string(), fmt_status));
+    }
+
+    // %m 输出代码中指定的日志信息
+    // %p 日志信息输出级别，及 DEBUG,INFO,ERROR等
+    // %n 换行符，用来给输出的每条日志进行换行；
+    // %r 输出自应用启动到输出该条Log信息所耗费的时间（以毫秒记）
+    // %t 输出产生该日志的线程全名
+    // %d 输出服务器的当前时间，默认格式为ISO8601（国际标准时间格式），也可以指定时间格式，如：%d{yyyy年MM月dd日 HH:mm:ss SSS}
+    // %I 输出日志发生的位置，包括类名，线程，及在代码中的行数，如：Test.main（Test.java:10）
+    // %F 输出日志消息产生时所在的文件名称
+    // %L 输出代码中的行号
+    // %% 输出一个"%"字符
+}
+//formatter子类items
+class MessageFormatItem : public LogFormatter::formatterItem{
+public:
+    void format(std::ostream& os, LogLevel::ptr level, LogEvent::ptr event) override{
+        os << event->getContent();
+    }
+};
+class LevelFormatItem : public LogFormatter::formatterItem{
+public:
+    void format(std::ostream& os, LogLevel::ptr level, LogEvent::ptr event) override{
+        os << LogLevel::to_string(level);
+    }
+};
 
 
 }
--- sylar/log.h
+++ sylar/log.h
@@ -10,13 +10,17 @@ namespace m_sylar{
 
 class LogLevel{
 public:
+    using ptr = std::shared_ptr(LogLevel);
     enum Level{
+        UNKNOWN = 0;
         DEBUG = 1;
         INFO = 2;
         WARN = 3;
         ERROR = 4;
         FATAL = 5;
     };
+
+    std::string to_string(LogLevel::Level);
 };
 
 //日志事件
@@ -25,24 +29,48 @@ public:
     typedef std::shared_ptr<LogEvent> ptr;
     LogEvent(){}
 
+    const char* getFileName() const{ return file_name;}
+    
+    int32_t getLine() const{ return m_line};
+    uint32_t getThreadID() const{ return m_threadID};
+    uint32_t getFiberId() const{ return m_fiberId;}
+    uint32_t getElapse() const{ return m_elapse;}
+    uint32_t getTimer() const{ return m_timer;}
+    std::string getContent () const{ return m_content;}
 private:
     const char* file_name = nullptr;    //日志名
     int32_t m_line = 0;                 //行号
-    int32_t m_threadID = 0;             //线程id
-    int32_t m_fiberID = 0;              //协程id
-    int32_t m_elapse = 0;               //程序启动时间（ms）
-    int64_t m_timer;                    //时间戳
+    uint32_t m_threadID = 0;             //线程id
+    uint32_t m_fiberID = 0;              //协程id
+    uint32_t m_elapse = 0;               //程序启动时间（ms）
+    uint64_t m_timer;                    //时间戳
     std::string m_content;
 };
+
 
 //日志格式
 class LogFormatter{
 public:
-    using ptr = std::shared_ptr<Formatter>;
-    std::string format(LogEvent::prt event);
-private:
+    using ptr = std::shared_ptr<LogFormatter>;
 
+    LogFormatter(const std::string& pattern);
+
+    std::string format(LogLevel::ptr level, LogEvent::prt event);
+
+public:
+    void init();            //初始化解析日志格式            
+    //格式items
+    class formatterItem{
+    public:
+        using ptr = std::shared_ptr<formatterItem>;
+        virtual ~formatterItem() {}
+        virtual void format(std::ostream& os, LogLevel::ptr level , LogEvent::ptr event) = 0; 
+    }
+private:
+    std::string m_pattern;
+    std::vector<formatterItem::ptr> m_items;
 };
+
 
 //日志输出地
 class LogAppender{
@@ -60,6 +88,7 @@ private:
     LogLevel::Level m_level;
     LogFormatter::ptr m_formatter;
 };
+
 
 //日志输出
 class Logger{
@@ -87,7 +116,6 @@ private:
     std::list<LogAppender::ptr> m_Appenders;
 };
 
-
 //控制台日志
 class StdoutLogAppender : public LogAppender{
 public:
@@ -95,6 +123,7 @@ public:
     void log(LogLevel::Level level, LogEvent::ptr event) override;
 
 };
+
 //文件日志
 class FileLogAppender : public LogAppender{
 public:
